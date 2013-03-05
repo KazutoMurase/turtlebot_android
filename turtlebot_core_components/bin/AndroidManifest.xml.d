5e859e188ed9bb54d2061768395ce7e44cf89958
KazutoMurase <kattyokaccho3616@gmail.com>
2013-03-05 11:09:15 -0800

    add necessary files to compile

---
+++ turtlebot_core_components/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/pr2/work/turtlebot_android/core_components/bin/AndroidManifest.xml : \
+/home/pr2/work/turtlebot_android/core_components/AndroidManifest.xml \
---
+++ turtlebot_core_components/gen/com/ros/turtlebot/apps/core_components/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ros.turtlebot.apps.core_components;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
