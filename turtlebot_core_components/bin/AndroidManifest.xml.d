d23bbe19c827a6a472394e2fc0d9a3c403778a90
KazutoMurase <kattyokaccho3616@gmail.com>
2013-03-05 11:10:56 -0800

    delete extra files

--- turtlebot_core_components/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/pr2/work/turtlebot_android/core_components/bin/AndroidManifest.xml : \
-/home/pr2/work/turtlebot_android/core_components/AndroidManifest.xml \
--- turtlebot_core_components/gen/com/ros/turtlebot/apps/core_components/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.ros.turtlebot.apps.core_components;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
